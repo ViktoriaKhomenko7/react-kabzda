8b1af25eecb431ec591c687f5effe14a5930c6ec
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-11-04 18:43:17 +0300

    training

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import {UncontrolledAccordion} from "./components/accordion/UncontrolledAccordion";
+// import {UnControlledRating} from "./components/rating/UnControlledRating";
+//import {OnnOff} from "./components/OnOff/OnnOff";
+import {UncontrolledAccordion} from "./components/SelfcontrolledAccordion/UncontrolledAccordion";
+import {UnControlledRating} from "./components/SelfControlledRating/UnControlledRating";
+import {OnnOff} from "./components/OnOff/OnnOff";
+import {Rating} from "./components/rating/Rating";
+import {Accordion} from "./components/accordion/Accordion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={"App"}>
+            <OnnOff/>
+            {/*//     <OnnOff on={false}/>*/}
+            {/*//     <OnnOff on={true}/>*/}
+            <UncontrolledAccordion titleValue={"Menu"}/>
+            {/*<UncontrolledAccordion titleValue={"Users"}/>*/}
+            <UnControlledRating/>
+            <Rating value={3}/>
+            <Accordion titleValue={"Menu"} collapsed={false}/>
+
+
+
+        </div>
+
+        // <div>
+        //     <UnControlledRating value={1}/>
+        //     <UnControlledRating value={2}/>
+        //     <UnControlledRating value={3}/>
+        //     <UnControlledRating value={4}/>
+        //     <UnControlledRating value={5}/>
+        // </div>
+    );
 }
+
+// type PageTitlePropsType = {
+//     title: string
+// }
+//
+// const PageTitle = (props: PageTitleProposType) => {
+//     return (
+//         <h1>--{props.title}--</h1>
+//     )
+// }
 
 export default App;
